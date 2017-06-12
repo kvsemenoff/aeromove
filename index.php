3138ef45566d30d45ae870ebdc8d0f94de019093
kvsemenoff <kvsemenoff@mail.ru>
2017-06-12 15:09:13 +0400

    f

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Главная</title>
+	<?php require_once('includes/header.php'); ?>
+</head>
+<body>
+	<div class="wrap">
+		
+	</div>
+	<div class="footer">
+		
+	</div>
+</body>
+</html>
