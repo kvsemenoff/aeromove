a7a7ec0332735586a8fe75f3ff0fc04a3109706c
boka <web_masters_07@mail.ru>
2017-06-12 16:42:12 +0300

    all

---
+++ includes/footer.php
@@ -0,0 +1,29 @@
+
+<!-- JS_BLOCK -->
+<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+<script src="libs/owl.carousel/owl.carousel.js"></script>
+<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+<script src="js/jquery.maskedinput.min.js"></script>
+<script src="js/fonts.js"></script>
+<script src="js/common.js"></script>
+
+<script src="block/inputext/script.js"></script>
+<script src="block/inpiutcheckbox/script.js"></script>
+<script src="block/textarea/script.js"></script>
+<script src="block/call-back/script.js"></script>
+<script src="block/common-title/script.js"></script>
+<script src="block/h1/script.js"></script>
+<script src="block/button/script.js"></script>
+<script src="block/card-item/script.js"></script>
+<script src="block/cadr-row/script.js"></script>
+<script src="block/pagintation/script.js"></script>
+<script src="block/socials/script.js"></script>
+<script src="block/header-top/script.js"></script>
+<script src="block/header-bottom-main/script.js"></script>
+<script src="block/header-botom-inner/script.js"></script>
+<script src="block/common-slider/script.js"></script>
+<script src="block/main-menu/script.js"></script>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,37 +1,17 @@
 <!doctype html>
 <html lang="ru">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Главная</title>
-	<?php require_once('includes/header.php'); ?>
-</head>
-<body>
-	<div class="wrapper">
-		<div class="content">
-			<div class="header-top">
-				<div class="logo-box">
-					<img src="img/logo.png" alt="AeroMovie" class="logo">
-				</div>
-				<div class="main-menu">
-					<ul>
-						<li><a href="#">Услуги</a></li>
-						<li><a href="#">Портфолио</a></li>
-						<li><a href="#">За кадром</a></li>
-						<li><a href="#">Блог</a></li>
-						<li><a href="#">Контакты</a></li>
-					</ul>
-				</div>
-				<div class="contact-box">
-					<span class="header-phone">+7 921 521-00-05</span>
-					<a href="#" class="call-back">Обратный звонок</a>
-				</div>
-			</div>
-		</div>
-		<div class="footer">
-			футер
-		</div>
-	</div>
-	
-</body>
-</html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Главная</title>
+<?php require_once('includes/header.php'); ?>
+<?php require_once('includes/header_top.php'); ?>
+<?php require_once('includes/header_bottom.php'); ?>
+<?php require_once('includes/second_menu.php'); ?>
+<?php require_once('includes/video.php'); ?>
+<?php require_once('includes/big.php'); ?>
+<?php require_once('includes/second_video.php'); ?>
+<?php require_once('includes/more.php'); ?>
+<?php require_once('includes/our.php'); ?>
+<?php require_once('includes/want.php'); ?>
+<?php require_once('includes/footer.php'); ?>
