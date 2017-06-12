35d8f9429d4850b355dbcf8b46f3f764af34614a
kvsemenoff <kvsemenoff@mail.ru>
2017-06-12 15:47:18 +0400

    d

--- index.php
+++ index.php
@@ -7,28 +7,31 @@
 	<?php require_once('includes/header.php'); ?>
 </head>
 <body>
-	<div class="wrap">
-		<div class="header-top">
-			<div class="logo-box">
-				<img src="img/logo.png" alt="AeroMovie" class="logo">
-			</div>
-			<div class="main-menu">
-				<ul>
-					<li><a href="#">Услуги</a></li>
-					<li><a href="#">Портфолио</a></li>
-					<li><a href="#">За кадром</a></li>
-					<li><a href="#">Блог</a></li>
-					<li><a href="#">Контакты</a></li>
-				</ul>
-			</div>
-			<div class="contact-box">
-				<span class="header-phone">+7 921 521-00-05</span>
-				<a href="#" class="call-back">Обратный звонок</a>
+	<div class="wrapper">
+		<div class="content">
+			<div class="header-top">
+				<div class="logo-box">
+					<img src="img/logo.png" alt="AeroMovie" class="logo">
+				</div>
+				<div class="main-menu">
+					<ul>
+						<li><a href="#">Услуги</a></li>
+						<li><a href="#">Портфолио</a></li>
+						<li><a href="#">За кадром</a></li>
+						<li><a href="#">Блог</a></li>
+						<li><a href="#">Контакты</a></li>
+					</ul>
+				</div>
+				<div class="contact-box">
+					<span class="header-phone">+7 921 521-00-05</span>
+					<a href="#" class="call-back">Обратный звонок</a>
+				</div>
 			</div>
 		</div>
+		<div class="footer">
+			футер
+		</div>
 	</div>
-	<div class="footer">
-		
-	</div>
+	
 </body>
 </html>
