aca3a24eeeca311fb86f59869896f4a7c7864022
Berzeg07 <berezg86@gmail.com>
2017-06-13 16:55:29 +0400

    13.06

---
+++ includes/second_video.php
@@ -0,0 +1,8 @@
+<div class="container">
+	<div class="video-box">
+		<div class="video-box__item">
+			<a href="#"><img src="img/play.png" alt=""></a>
+			<p>Посмотрите наше портфолио за 2016 год!</p>
+		</div>
+	</div>
+</div>
