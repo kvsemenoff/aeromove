829529108c9853a3b574e778bbc4568a7121e203
engine77 <zaur.1kager@gmail.com>
2017-06-13 18:24:00 +0400

    inputs

---
+++ includes/big.php
@@ -0,0 +1,194 @@
+<div class="container">
+	<div class="bigpark">
+		<h1 class="common-title_digital bigpark__caption">Самый большой парк оборудования <br> в Санкт-Петербурге    <span>02</span></h1>
+
+		<div class="bigpark__leftbox">
+			<div class="bigpark__tab">
+				<div class="bigpark__img">
+					<img src="img/copter1.png" alt="">
+				</div>
+				<div class="bigpark__txtbox">
+					<h4>Квадрокоптер <b>DJI Phantom 4 Pro</b></h4>
+					<p>Самый простой вариант для съемки. Рекомендуется для фотосъемки объектов
+						и территорий, съемки малобюджетных видеороликов. Управляется одним пилотом,
+						непригоден для съемки любой динамики, только простые пролеты.</p>
+				</div>				
+			</div>
+			<div class="bigpark__button">
+				<a class="button button_1" href="#">Заказать видеосъемку</a>
+			</div>
+		</div>
+
+		<div class="bigpark__rightbox">
+			<h4>Список оборудования и стоимость:</h4>
+			<div class="bigpark__accordeon">
+				<div class="bigpark__accordeon-item">
+					<div class="bigpark__accordeon-img">
+						<img src="img/coptersm1.png" alt="">
+					</div>
+					<span class="bigpark__accordeon-nameproduct">DJI Phantom 4</span>
+					<div class="bigpark__accordeon-pricebox">
+						<span>от <b>5.000</b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+					</div>
+					<div class="clearfix"></div>
+					<div class="bigpark__accordeon-hiddenbox">
+						<div class="bigpark__accordeon-characteristics">
+							<div class="bigpark__accordeon-hourprice">
+								<em>Стоимость:</em>
+								<span>от 2 часов - <b>5 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+								<span>до 8 часов - <b>8 000 </b><i class="fa fa-rub" aria-hidden="true"></i></span>
+								<span>смена 8-10 часов - <b>10 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+							</div>
+							<div class="bigpark__accordeon-tech">
+								<em>Тех. характеристики:</em>
+								<span>объектив 20 mm;</span>
+								<span>видео FHD fps 120,</span>
+								<span>4K fps 50</span>
+							</div>
+						</div>
+						<div class="bigpark__accordeon-video">
+							<a href="#">
+								<img src="img/bigparkimg.jpg" alt="">
+							</a>
+						</div>
+						<div class="clearfix"></div>							
+					</div>
+				</div>
+				<div class="bigpark__accordeon-item">
+					<div class="bigpark__accordeon-img">
+						<img src="img/coptersm2.png" alt="">
+					</div>
+					<span class="bigpark__accordeon-nameproduct">DJi Inspire Zenmuse X3</span>
+					<div class="bigpark__accordeon-pricebox">
+						<span>от <b>10.000</b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+					</div>
+					<div class="clearfix"></div>
+					<div class="bigpark__accordeon-hiddenbox">
+						<div class="bigpark__accordeon-characteristics">
+							<div class="bigpark__accordeon-hourprice">
+								<em>Стоимость:</em>
+								<span>от 2 часов - <b>5 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+								<span>до 8 часов - <b>8 000 </b><i class="fa fa-rub" aria-hidden="true"></i></span>
+								<span>смена 8-10 часов - <b>10 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+							</div>
+							<div class="bigpark__accordeon-tech">
+								<em>Тех. характеристики:</em>
+								<span>объектив 20 mm;</span>
+								<span>видео FHD fps 120,</span>
+								<span>4K fps 50</span>
+							</div>
+						</div>
+						<div class="bigpark__accordeon-video">
+							<a href="#">
+								<img src="img/bigparkimg.jpg" alt="">
+							</a>
+						</div>
+						<div class="clearfix"></div>							
+					</div>
+				</div>
+				<div class="bigpark__accordeon-item">
+					<div class="bigpark__accordeon-img">
+						<img src="img/coptersm3.png" alt="">
+					</div>
+					<span class="bigpark__accordeon-nameproduct">DJI NSPIRE X5, X5R</span>
+					<div class="bigpark__accordeon-pricebox">
+						<span>от <b>15.000</b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+					</div>
+					<div class="clearfix"></div>
+					<div class="bigpark__accordeon-hiddenbox">
+						<div class="bigpark__accordeon-characteristics">
+							<div class="bigpark__accordeon-hourprice">
+								<em>Стоимость:</em>
+								<span>от 2 часов - <b>5 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+								<span>до 8 часов - <b>8 000 </b><i class="fa fa-rub" aria-hidden="true"></i></span>
+								<span>смена 8-10 часов - <b>10 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+							</div>
+							<div class="bigpark__accordeon-tech">
+								<em>Тех. характеристики:</em>
+								<span>объектив 20 mm;</span>
+								<span>видео FHD fps 120,</span>
+								<span>4K fps 50</span>
+							</div>
+						</div>
+						<div class="bigpark__accordeon-video">
+							<a href="#">
+								<img src="img/bigparkimg.jpg" alt="">
+							</a>
+						</div>
+						<div class="clearfix"></div>							
+					</div>
+				</div>
+				<div class="bigpark__accordeon-item">
+					<div class="bigpark__accordeon-img">
+						<img src="img/coptersm4.png" alt="">
+					</div>
+					<span class="bigpark__accordeon-nameproduct">DJI MATRICE 600 <i> и </i> SONY ALPHA A4S II</span>
+					<div class="bigpark__accordeon-pricebox">
+						<span>от <b>15.000</b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+					</div>
+					<div class="clearfix"></div>
+					<div class="bigpark__accordeon-hiddenbox">
+						<div class="bigpark__accordeon-characteristics">
+							<div class="bigpark__accordeon-hourprice">
+								<em>Стоимость:</em>
+								<span>от 2 часов - <b>5 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+								<span>до 8 часов - <b>8 000 </b><i class="fa fa-rub" aria-hidden="true"></i></span>
+								<span>смена 8-10 часов - <b>10 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+							</div>
+							<div class="bigpark__accordeon-tech">
+								<em>Тех. характеристики:</em>
+								<span>объектив 20 mm;</span>
+								<span>видео FHD fps 120,</span>
+								<span>4K fps 50</span>
+							</div>
+						</div>
+						<div class="bigpark__accordeon-video">
+							<a href="#">
+								<img src="img/bigparkimg.jpg" alt="">
+							</a>
+						</div>
+						<div class="clearfix"></div>							
+					</div>
+				</div>
+				<div class="bigpark__accordeon-item">
+					<div class="bigpark__accordeon-img">
+						<img src="img/coptersm4.png" alt="">
+					</div>
+					<span class="bigpark__accordeon-nameproduct">DJI matrice 600 <i>и</i> red</span>
+					<div class="bigpark__accordeon-pricebox">
+						<span>от <b>15.000</b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+					</div>
+					<div class="clearfix"></div>
+					<div class="bigpark__accordeon-hiddenbox">
+						<div class="bigpark__accordeon-characteristics">
+							<div class="bigpark__accordeon-hourprice">
+								<em>Стоимость:</em>
+								<span>от 2 часов - <b>5 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+								<span>до 8 часов - <b>8 000 </b><i class="fa fa-rub" aria-hidden="true"></i></span>
+								<span>смена 8-10 часов - <b>10 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+							</div>
+							<div class="bigpark__accordeon-tech">
+								<em>Тех. характеристики:</em>
+								<span>объектив 20 mm;</span>
+								<span>видео FHD fps 120,</span>
+								<span>4K fps 50</span>
+							</div>
+						</div>
+						<div class="bigpark__accordeon-video">
+							<a href="#">
+								<img src="img/bigparkimg.jpg" alt="">
+							</a>
+						</div>
+						<div class="clearfix"></div>							
+					</div>
+				</div>
+			</div>
+			<p class="bigpark__textbottom">Цена на проведение съемки может меняться в зависимости от требований и параметров съемки. Окончательную цену уточняйте у менеджера.</p>	
+
+
+
+
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
