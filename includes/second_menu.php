9285ec9558a3d97d112efe1b0ccb1da411f9d65e
Berzeg07 <berezg86@gmail.com>
2017-06-13 16:25:24 +0400

    13.06

---
+++ includes/second_menu.php
@@ -0,0 +1,12 @@
+<div class="second-menu">
+	<div class="container">
+		<ul class="second-menu__box">
+			<li><a href="#">Видеосъемка</a></li>
+			<li><a href="#">Фотосъемка</a></li>
+			<li><a href="#">Аренда оборудования</a></li>
+			<li><a href="#">Виртуальные туры</a></li>
+			<li><a href="#">Онлайн трансляции</a></li>
+			<li><a href="#">Пост-продакшн</a></li>
+		</ul>
+	</div>
+</div>
