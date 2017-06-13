2a7583392fae57c337b8113baa223b32e68db4e9
Berzeg07 <berezg86@gmail.com>
2017-06-13 18:01:12 +0400

    13.06

--- js/common.js
+++ js/common.js
@@ -2,6 +2,32 @@
 
 $(document).ready(function(){
 
+
+     var owl2 = $("#our-slider");
+        owl2.owlCarousel({
+            loop:true,
+            nav:true, 
+            autoplay:false,
+            smartSpeed:1000,
+            margin:0,
+        center:false,     //если нужны обрезаные края
+        navText:['<span class="nav-left"></span>','<span class="nav-right"></span>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2 
+            },
+            768:{
+                items:3
+            },  
+              1240:{
+                items:4
+            },     
+        }
+    });
+
 	// Переключатель кол-ва товара в корзине (legatkani)
 	var itemprice = $('.item-price').text();
 	$('#total').val(itemprice);
