bc2cec748684d89cf52870538fdc2603f5df9674
Berzeg07 <berezg86@gmail.com>
2017-06-13 15:57:26 +0400

    13.06

---
+++ includes/header_bottom2.php
@@ -0,0 +1,11 @@
+<div class="header_bottom header_bottom2">
+
+	<div class="header-bottom__box">
+		<div class="container">
+			<h2>За кадром</h2>
+			<span>Фотографии со съемочной площадки.</span>
+		</div>
+	</div>
+	
+
+</div>	
