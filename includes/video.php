a79ec9b73fd7fc0fdf9bfd14e936d2966818bb1b
engine77 <zaur.1kager@gmail.com>
2017-06-13 14:36:14 +0400

    inputs

---
+++ includes/video.php
@@ -0,0 +1,25 @@
+<div class="videobgimg">
+	<div class="container">
+		<div class="video">
+			<div class="video__leftbox">
+				<h1 class="common-title_digital">Проведение <br> видеосъемки <span>01</span></h1>
+				<div class="video__txt">
+					<p>Видеосъемка с воздуха — наша основная специализация. У нас есть коптеры, способные решать разные задачи и с разным бюджетом. </p>
+					<p>Небольшие квадрокоптеры для съемок малобюджетных роликов, тяжелый гексакоптер с профессиональными камерами и каосильный коптер с подвесом на камеры линейки RED, который принимает участие
+						в съемках большого кино.</p>
+				</div>
+				<div class="video__buttons">				
+					<a class="button button_2" href="#">Заказать видеосъемку</a>
+					<a href="#" class="video__lookmore">Подробнее в разделе “Аэровидеосъемка”</a>
+				</div>
+			</div>
+			<div class="video__rightbox">
+				<div class="video__imgbox"><img src="img/video1.jpg" alt=""></div>
+				<div class="video__imgbox"><img src="img/video2.jpg" alt=""></div>
+			</div>
+
+
+
+		</div>
+	</div>
+</div>
