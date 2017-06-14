38a2f3ef880ed0294bd30d6a4d58e3272a42ba92
boka <web_masters_07@mail.ru>
2017-06-14 13:03:15 +0300

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,56 +1,60 @@
 <div class="footer">
 	<div class="footer-top">
 		<div class="container">
-			<div class="col-md-3 col-sm-6 col-xs-6">
-				<span>Основной</span>
-				<ul>
-					<li><a href="#">Портфолио</a></li>
-					<li><a href="#">За кадром</a></li>
-					<li><a href="#">Блог</a></li>
-					<li><a href="#">F.A.Q</a></li>
-					<li><a href="#">Команда</a></li>
+			<div class="row">
+				<div class="col-md-3 col-sm-6 col-xs-6">
+					<span>Основной</span>
+					<ul>
+						<li><a href="#">Портфолио</a></li>
+						<li><a href="#">За кадром</a></li>
+						<li><a href="#">Блог</a></li>
+						<li><a href="#">F.A.Q</a></li>
+						<li><a href="#">Команда</a></li>
 
-				</ul>
-			</div>
-			<div class="col-md-3 col-sm-6 col-xs-6">
-				<span>Дополнительно</span>
-				<ul>
-					<li><a href="#">Контакты</a></li>
-					<li><a href="#">Вакансии</a></li>
-					<li><a href="#">Презентация</a></li>
-					<li><a href="#">Заказать звонок</a></li>
-				</ul>		
-			</div>
-			<div class="col-md-3 col-sm-6 col-xs-6">
-				<p>
-					<span>Контакты</span><br>
-					+7 (921) 562-00-05 <br>	
-					contact@aero-movie.ru
-				</p>
-				<div class="icon">
-					<a href="#"><span class="facebook"></span></a>
-					<a href="#"><span class="instagram"></span></a>					
-					<a href="#"><span class="youtube"></span></a>						
-					<a href="#"><span class="twitter"></span></a>
+					</ul>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-6">
+					<span>Дополнительно</span>
+					<ul>
+						<li><a href="#">Контакты</a></li>
+						<li><a href="#">Вакансии</a></li>
+						<li><a href="#">Презентация</a></li>
+						<li><a href="#">Заказать звонок</a></li>
+					</ul>		
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-6">
+					<p>
+						<span>Контакты</span><br>
+						+7 (921) 562-00-05 <br>	
+						contact@aero-movie.ru
+					</p>
+					<div class="icon">
+						<a href="#"><span class="facebook"></span></a>
+						<a href="#"><span class="instagram"></span></a>					
+						<a href="#"><span class="youtube"></span></a>						
+						<a href="#"><span class="twitter"></span></a>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-6">
+					<p>
+						<span>Адрес офиса</span><br>
+						Каменоостровский проспект 26-28, пом. 2
+						с 10:00 до 19:00 (лучше звонить заранее)
+					</p>
 				</div>
 			</div>
-			<div class="col-md-3 col-sm-6 col-xs-6">
-				<p>
-					<span>Адрес офиса</span><br>
-					Каменоостровский проспект 26-28, пом. 2
-					с 10:00 до 19:00 (лучше звонить заранее)
-				</p>
+		</div>
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<p class="copyright">AERO-MOVIE.RU / летаем с 2013 года. Копирование контента запрещено.</p>
+					</div>
+				</div>
+				<div class="clearfix"></div>
 			</div>
 		</div>
 	</div>
-	<div class="footer-bottom">
-		<div class="container">
-			<div class="col-md-12">
-				<p class="copyright">AERO-MOVIE.RU / летаем с 2013 года. Копирование контента запрещено.</p>
-			</div>
-			<div class="clearfix"></div>
-		</div>
-	</div>	
 </div>
 
 <!-- scripts start -->
