bdfb8bad407c16ebce2626782061154166353819
Berzeg07 <berezg86@gmail.com>
2017-06-14 12:10:57 +0400

    14.06

--- 404.php
+++ 404.php
@@ -5,12 +5,28 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 	<?php require_once('includes/header.php'); ?>
+
 	<div class="wrapper">
 		<div class="content">
-			<?php require_once('includes/header-top.php'); ?>
+
+			<div class="container">
+				<div class="logo404">
+					<img src="img/log404.png" alt="">
+				</div>
+				<div class="error-box">
+					<span>404</span>
+					<div class="error-box__img">
+						<img class="img-responsive" src="img/copter.png" alt="">
+					</div>
+					<p>Что-то пошло не так и страница исчезла</p>
+				</div>
+			</div>
+
 		</div>
 		<div class="footer">
-			<?php require_once('includes/footer.php'); ?>
+			<div class="footer404">
+				<p>Мы вернем вас на главную через несколько секунд. Нажмите <a href="#">сюда</a>, если не хотите ждать.</p>
+			</div>
 		</div>
 	</div>
 </body>
