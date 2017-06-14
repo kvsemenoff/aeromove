16c3d24ef1373d755b3c8a6bc5aa487470d317bf
Berzeg07 <berezg86@gmail.com>
2017-06-14 10:54:42 +0400

    14.06

--- includes/header_bottom2.php
+++ includes/header_bottom2.php
@@ -4,6 +4,12 @@
 		<div class="container">
 			<h2>За кадром</h2>
 			<span>Фотографии со съемочной площадки.</span>
+
+			<ul class="pagination">
+				<li><a href="#">Главная /</a></li>
+				<li><a href="#">Блог /</a></li>
+				<li><span>Статья</span></li>
+			</ul>
 		</div>
 	</div>
 	
