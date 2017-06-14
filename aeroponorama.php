6179ce4e2b111eef3b8747ed857c154f3f55d836
engine77 <zaur.1kager@gmail.com>
2017-06-14 17:59:52 +0400

    service

--- aeroponorama.php
+++ aeroponorama.php
@@ -1,17 +1,142 @@
 <!doctype html>
- 						<html lang="ru">
- 						<head>
- 							<meta charset="UTF-8">
- 							<meta name="viewport" content="width=device-width, initial-scale=1">
- 							<title></title>
-<?php require_once('includes/header.php'); ?>
- 						<div class="wrapper">
- 							<div class="content">
- 								<?php require_once('includes/header-top.php'); ?>
- 							</div>
- 							<div class="footer">
- 								<?php require_once('includes/footer.php'); ?>
- 							</div>
- 						</div>
- 						</body>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<?php require_once('includes/header.php'); ?>
+	<div class="wrapper">
+		<div class="content">
+			<?php require_once('includes/header_top.php'); ?>
+			<div class="header_bottom header_bottom2">
+				<div class="header-bottom__box">
+					<div class="container">
+						<h2>Виртуальные 3D туры с квадрокоптера</h2>
+						<span>Съемка аэропанорамы, компиляция и инфографика.</span>
+						<?php require_once('includes/breadcrumbs.php'); ?>
+					</div>
+				</div>
+			</div>
+
+			<div class="shooting">	
+				<div class="container">
+					<div class="shooting_block">				
+						<div class="shooting__leftbox">
+							<h1 class="common-title">Условия аренды</h1>
+							<div class="shooting__txtbox">
+								<p>Создание виртуальных туров 360 (3D) производится с помощью аэросъемки, с дальнейшей компиляцией панорамы и установкой шотов с инфографикой.</p>
+								<p>Чаще всего используется для сайтов, презентаций и VR очков.</p>
+								<p>Панорама имеет высокое разрешение, возможность переключения уровня детализации (в зависимости от мощности компьютера) и полный интерактив с пользователем.</p>
+								<p>Возможно нанесение инфографиик на панораму.</p>
+							</div>
+							<div class="shooting__button">
+								<a class="button button_2" href="#">Заказать панораму</a>
+							</div>
+						</div>
+						<div class="shooting__rightbox">
+							<div class="common-slider">
+								<div class="slider-owl-carousel">						
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>		
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>				
+								</div>
+								<div class="common-slider__txtbox">
+									<div class="common-slider__numberbox">
+										<span class="number-active">1</span>
+										<i>/</i>
+										<span class="number-all">9</span>
+									</div>
+									<p class="common-slider__txt">Проведение аэрофотосъемки ЖК в Сертолово для компании “Балтйский лизинг”</p>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			
+			<div class="panorams">
+				<div class="container">
+					<h1 class="common-title">Панорамы</h1>
+					<ul class="panorams__tabs">
+						<li><a href="panoramstab1" class="panorams__acivetab">Земельный участок</a></li>
+						<li><a href="panoramstab2">Загородный дом</a></li>
+						<li><a href="panoramstab3">Жилой комплекс</a></li>
+						<li><a href="panoramstab4">Инфраструктура</a></li>
+					</ul>
+					<div class="clearfix"></div>                    		
+					<div class="panorams__content">
+						<div class="panorams__img panoramstab1">
+							<img src="img/panoram1.jpg" alt="">
+						</div>
+						<div class="panorams__img panoramstab2">
+							<img src="img/panoram1.jpg" alt="">
+							2
+						</div>
+						<div class="panorams__img panoramstab3">
+							<img src="img/panoram1.jpg" alt="">
+							3
+						</div>
+						<div class="panorams__img panoramstab4">
+							<img src="img/panoram1.jpg" alt="">
+							4
+						</div>
+					</div>
+				</div>
+			</div>	
+
+
+
+
+
+
+
+
+
+
+
+		</div>
+		<div class="footer">
+			<?php require_once('includes/footer.php'); ?>
+		</div>
+	</div>
+</body>
 </html>
