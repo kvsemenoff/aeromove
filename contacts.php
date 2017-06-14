5f209b7a77b2ae054ca8c5a2bef2fdab43751dab
Berzeg07 <berezg86@gmail.com>
2017-06-14 13:50:46 +0400

    14.06

--- contacts.php
+++ contacts.php
@@ -3,18 +3,52 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<title></title>
+
 	<?php require_once('includes/header.php'); ?>
+
 	<div class="wrapper">
 		<div class="content">
-		<?php require_once('includes/header_top.php'); ?>
-			<div class="contact-box">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d999.4471078296654!2d30.276294031892824!3d59.933897480328845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x469630df4fdd0719%3A0x91eb06742a292036!2snaberezhnaya+Leytenanta+Shmidta%2C+23%2C+Sankt-Peterburg%2C+199034!5e0!3m2!1sen!2sru!4v1497429066598" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+			<?php require_once('includes/header_top.php'); ?>
+			<div class="container">
+				<div class="contact-box">
+
+					<ul class="pagination pagination__contact">
+						<li><a href="#">Главная /</a></li>							
+						<li><span>Контакты</span></li>
+					</ul>
+
+					<h1>Контакты</h1>
+
+					<div class="contact-box__item">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d999.4471078296654!2d30.276294031892824!3d59.933897480328845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x469630df4fdd0719%3A0x91eb06742a292036!2snaberezhnaya+Leytenanta+Shmidta%2C+23%2C+Sankt-Peterburg%2C+199034!5e0!3m2!1sen!2sru!4v1497429066598" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+					</div>
+
+					<div class="contact-box__item contact-box__item_2">
+						<h1>+7 (921) 562-00-05</h1>
+						<p class="cp1">Санкт-Петербург, наб. Лейтенанта Шмидта 23</p>
+						<p class="cp2">Пн - сб с 10:00 до 19:00 <b>(звоните заранее)</b></p>
+						<p class="cp3">contact@aero-movie.ru</p>
+						<div class="contact-box__item_social">
+							<span>Мы в социальных сетях:</span>
+							<ul>
+								<li><a href="#"><img src="img/f1.png" alt=""></a></li>
+								<li><a href="#"><img src="img/f2.png" alt=""></a></li>
+								<li><a href="#"><img src="img/f3.png" alt=""></a></li>
+								<li><a href="#"><img src="img/f4.png" alt=""></a></li>
+							</ul>
+						</div>
+					</div>
+
+				</div>
 			</div>
 		</div>
+
 		<div class="footer">
 			<?php require_once('includes/footer.php'); ?>
 		</div>
+
 	</div>
 </body>
 </html>
