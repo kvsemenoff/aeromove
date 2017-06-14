4f745dc7d62b1790f0ef9f7e4f1b4ba74d156328
engine77 <zaur.1kager@gmail.com>
2017-06-14 11:36:21 +0400

    inputs

--- services.php
+++ services.php
@@ -3,11 +3,101 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
+	<title>Ролики под ключ и пост-продакшн</title>
 	<?php require_once('includes/header.php'); ?>
 	<div class="wrapper">
 		<div class="content">
-			<?php require_once('includes/header-top.php'); ?>
+			<?php require_once('includes/header_top.php'); ?>
+			<div class="header_bottom header_bottom2 servicesbg">
+				<div class="header-bottom__box">
+					<div class="container">
+						<h2>Ролики под ключ и пост-продакшн</h2>
+						<span>Видеомонтаж, инфографика и VFX</span>
+						<ul class="pagination">
+							<li><a href="#">Главная /</a></li>							
+							<li><span>Продакшн</span></li>
+						</ul>
+					</div>
+				</div>
+			</div>	
+			
+			<div class="shooting">	
+				<div class="container shooting_block">				
+					<div class="shooting__leftbox">
+						<h1 class="common-title">Съемки ролика под ключ</h1>
+						<div class="shooting__txtbox">
+							<p>Помимо съемок с квадрокоптеров у нас есть техническая база для производства рекламных и имиджевых видеороликов “под ключ”.</p>
+							<p>В нашей команде есть все основные специалисты по производству: режиссер, сценаристы, профессиональные операторы, специалисты по инфографике, 3D-моделям и VFX.</p>
+						</div>
+						<div class="shooting__button">
+							<a class="button button_2" href="#">Заказать съемку</a>
+						</div>
+					</div>
+					<div class="shooting__rightbox">
+						<div class="common-slider">
+							<div class="slider-owl-carousel">
+								<div class="item">
+									<div class="common-slider__img">
+										<img src="img/sliderimg1.jpg" alt="">
+									</div>			
+								</div>
+								<div class="item">
+									<div class="common-slider__img">
+										<img src="img/sliderimg1.jpg" alt="">
+									</div>			
+								</div>
+								<div class="item">
+									<div class="common-slider__img">
+										<img src="img/sliderimg1.jpg" alt="">
+									</div>			
+								</div>
+								<div class="item">
+									<div class="common-slider__img">
+										<img src="img/sliderimg1.jpg" alt="">
+									</div>			
+								</div>
+								<div class="item">
+									<div class="common-slider__img">
+										<img src="img/sliderimg1.jpg" alt="">
+									</div>			
+								</div>
+								<div class="item">
+									<div class="common-slider__img">
+										<img src="img/sliderimg1.jpg" alt="">
+									</div>			
+								</div>		
+								<div class="item">
+									<div class="common-slider__img">
+										<img src="img/sliderimg1.jpg" alt="">
+									</div>			
+								</div>
+								<div class="item">
+									<div class="common-slider__img">
+										<img src="img/sliderimg1.jpg" alt="">
+									</div>			
+								</div>
+								<div class="item">
+									<div class="common-slider__img">
+										<img src="img/sliderimg1.jpg" alt="">
+									</div>			
+								</div>				
+							</div>
+							<div class="common-slider__txtbox">
+								<div class="common-slider__numberbox">
+									<span class="number-active">1</span>
+									<i>/</i>
+									<span class="number-all">9</span>
+								</div>
+								<p class="common-slider__txt">Проведение аэрофотосъемки ЖК в Сертолово для компании “Балтйский лизинг”</p>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+					</div>
+					
+
+				</div>
+			</div>
+
 		</div>
 		<div class="footer">
 			<?php require_once('includes/footer.php'); ?>
