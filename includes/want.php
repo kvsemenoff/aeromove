c77b6285d2bab3caeb382a0c663ccdae7bb4dba4
boka <web_masters_07@mail.ru>
2017-06-15 12:06:10 +0300

    want

---
+++ includes/want.php
@@ -0,0 +1,12 @@
+<div class="want">
+	<div class="container">
+		<div class="title">
+			<div class="our-wrap__title margin">
+				<h1 class="common-title_digital white">Хотите работать с нами? <span>05</span></h1>
+			</div>
+			<p>Заполните форму заказа или закажите обратный звонок. 
+				Мы свяжемся с вами в течение 5 минут.</p>
+				<img src="img/dron.png" alt="img" class="img-responsive">
+			</div>
+		</div>
+	</div>
