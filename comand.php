acf2ce8a48b1f4ca8c17849b53cc4e00c846247b
boka <web_masters_07@mail.ru>
2017-06-16 18:21:14 +0300

    comand

--- comand.php
+++ comand.php
@@ -9,48 +9,165 @@
 		<div class="content">
 			<?php require_once('includes/header_top.php'); ?>
 			<?php require_once('includes/header_bottom2.php'); ?>
-			<div class="comand">
-				<div class="container">
-					<div class="comand-block">
-						<h2>Аэросъемка. Основной состав</h2>
-						<div class="col-md-3">
-							<div class="comand1">
-								<img src="img/comand.jpg" alt="img">
+			<div class="comand-box">
+				<div class="comand">
+					<div class="container">
+						<div class="comand-block">
+							<div class="col-md-12">
+								<h2>Аэросъемка. Основной состав</h2>
+							</div>	
+							<div class="clearfix"></div>						
+							<div class="col-md-3">
+								<div class="comand1">
+									<img src="img/comand.jpg" alt="img" class="img-responsive">
+								</div>
+								<p>
+									Намоконов Денис<br>
+									<span>Видеооператор на коптере</span>
+								</p>
 							</div>
-							<p>
-								Намоконов Денис<br>
-								<span>Видеооператор на коптере</span>
-							</p>
-						</div>
-						<div class="col-md-3">
-							<div class="comand2">
-								<img src="img/comand.jpg" alt="img">
+							<div class="col-md-3">
+								<div class="comand2">
+									<img src="img/comand.jpg" alt="img" class="img-responsive">
+								</div>
+								<p>
+									Намоконов Денис<br>
+									<span>Видеооператор на коптере</span>
+								</p>
 							</div>
-							<p>
-								Намоконов Денис<br>
-								<span>Видеооператор на коптере</span>
-							</p>
-						</div>
-						<div class="col-md-3">
-							<div class="comand3">
-								<img src="img/comand.jpg" alt="img">
+							<div class="col-md-3">
+								<div class="comand3">
+									<img src="img/comand.jpg" alt="img" class="img-responsive">
+								</div>
+								<p>
+									Намоконов Денис<br>
+									<span>Видеооператор на коптере</span>
+								</p>
 							</div>
-							<p>
-								Намоконов Денис<br>
-								<span>Видеооператор на коптере</span>
-							</p>
-						</div>
-						<div class="col-md-3">
-							<div class="comand4">
-								<img src="img/comand.jpg" alt="img">
+							<div class="col-md-3">
+								<div class="comand4">
+									<img src="img/comand.jpg" alt="img" class="img-responsive">
+								</div>
+								<p>
+									Намоконов Денис<br>
+									<span>Видеооператор на коптере</span>
+								</p>
 							</div>
-							<p>
-								Намоконов Денис<br>
-								<span>Видеооператор на коптере</span>
-							</p>
-						</div>
-						<div class="clearfix"></div>
-					</div>		
+							<div class="clearfix"></div>
+						</div>		
+					</div>
+				</div>
+				<div class="comand">
+					<div class="container">
+						<div class="comand-block">
+							<div class="col-md-12">
+								<h2>Постпродакшн. Операторская группа</h2>
+							</div>	
+							<div class="clearfix"></div>						
+							<div class="col-md-3">
+								<div class="comand1">
+									<img src="img/comand.jpg" alt="img" class="img-responsive">
+								</div>
+								<p>
+									Намоконов Денис<br>
+									<span>Видеооператор на коптере</span>
+								</p>
+							</div>
+							<div class="col-md-3">
+								<div class="comand2">
+									<img src="img/comand.jpg" alt="img" class="img-responsive">
+								</div>
+								<p>
+									Намоконов Денис<br>
+									<span>Видеооператор на коптере</span>
+								</p>
+							</div>
+							<div class="col-md-3">
+								<div class="comand3">
+									<img src="img/comand.jpg" alt="img" class="img-responsive">
+								</div>
+								<p>
+									Намоконов Денис<br>
+									<span>Видеооператор на коптере</span>
+								</p>
+							</div>
+							<div class="col-md-3">
+								<div class="comand4">
+									<img src="img/comand.jpg" alt="img" class="img-responsive">
+								</div>
+								<p>
+									Намоконов Денис<br>
+									<span>Видеооператор на коптере</span>
+								</p>
+							</div>
+							<div class="clearfix"></div>
+						</div>		
+					</div>
+				</div>
+				<div class="comand">
+					<div class="container">
+						<div class="comand-block">
+							<div class="col-md-12">
+								<h2>Постпродакшн. Администраторы</h2>
+							</div>	
+							<div class="clearfix"></div>						
+							<div class="col-md-3">
+								<div class="comand1">
+									<img src="img/comand.jpg" alt="img" class="img-responsive">
+								</div>
+								<p>
+									Намоконов Денис<br>
+									<span>Видеооператор на коптере</span>
+								</p>
+							</div>
+							<div class="col-md-3">
+								<div class="comand2">
+									<img src="img/comand.jpg" alt="img" class="img-responsive">
+								</div>
+								<p>
+									Намоконов Денис<br>
+									<span>Видеооператор на коптере</span>
+								</p>
+							</div>
+							<div class="col-md-3">
+								<div class="comand3">
+									<img src="img/comand.jpg" alt="img" class="img-responsive">
+								</div>
+								<p>
+									Намоконов Денис<br>
+									<span>Видеооператор на коптере</span>
+								</p>
+							</div>
+							<div class="col-md-3">
+								<div class="comand4">
+									<img src="img/comand.jpg" alt="img" class="img-responsive">
+								</div>
+								<p>
+									Намоконов Денис<br>
+									<span>Видеооператор на коптере</span>
+								</p>
+							</div>
+							<div class="clearfix"></div>
+							<div class="col-md-3">
+								<div class="comand1">
+									<img src="img/comand.jpg" alt="img" class="img-responsive">
+								</div>
+								<p>
+									Намоконов Денис<br>
+									<span>Видеооператор на коптере</span>
+								</p>
+							</div>
+							<div class="col-md-3">
+								<div class="comand2">
+									<img src="img/comand.jpg" alt="img" class="img-responsive">
+								</div>
+								<p>
+									Намоконов Денис<br>
+									<span>Видеооператор на коптере</span>
+								</p>
+							</div>
+						</div>		
+					</div>
 				</div>
 			</div>
 			<?php require_once('includes/want.php'); ?>
