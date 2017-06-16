de8b0acfdf1515c52cfc0b10e5cc7a26942b8800
engine77 <zaur.1kager@gmail.com>
2017-06-16 20:41:33 +0400

    aerophoto

--- prodacshn.php
+++ prodacshn.php
@@ -1,17 +1,167 @@
 <!doctype html>
- 						<html lang="ru">
- 						<head>
- 							<meta charset="UTF-8">
- 							<meta name="viewport" content="width=device-width, initial-scale=1">
- 							<title></title>
-<?php require_once('includes/header.php'); ?>
- 						<div class="wrapper">
- 							<div class="content">
- 								<?php require_once('includes/header-top.php'); ?>
- 							</div>
- 							<div class="footer">
- 								<?php require_once('includes/footer.php'); ?>
- 							</div>
- 						</div>
- 						</body>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Ролики под ключ и пост-продакшн</title>
+	<?php require_once('includes/header.php'); ?>
+	<div class="wrapper">
+		<div class="content">
+			<?php require_once('includes/header_top.php'); ?>
+			<?php require_once('includes/header_bottom2.php'); ?>
+			
+			<div class="shooting">	
+				<div class="container">
+					<div class="shooting_block">				
+						<div class="shooting__leftbox">
+							<h1 class="common-title">Съемки ролика под ключ</h1>
+							<div class="shooting__txtbox">
+								<p>Помимо съемок с квадрокоптеров у нас есть техническая база для производства рекламных и имиджевых видеороликов “под ключ”.</p>
+								<p>В нашей команде есть все основные специалисты по производству: режиссер, сценаристы, профессиональные операторы, специалисты по инфографике, 3D-моделям и VFX.</p>
+							</div>
+							<div class="shooting__button">
+								<a class="button button_2" href="#">Заказать съемку</a>
+							</div>
+						</div>
+						<div class="shooting__rightbox">
+							<div class="common-slider">
+								<div class="slider-owl-carousel">
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>		
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>				
+								</div>
+								<div class="common-slider__txtbox">
+									<div class="common-slider__numberbox">
+										<span class="number-active">1</span>
+										<i>/</i>
+										<span class="number-all">9</span>
+									</div>
+									<p class="common-slider__txt">Проведение аэрофотосъемки ЖК в Сертолово для компании “Балтйский лизинг”</p>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="examplesblock">
+				<div class="container">
+					<h1 class="common-title">Примеры проектов под ключ</h1>
+
+					<div class="examplesblock__box">
+						<div class="examplesblock__left">
+							<div class="examplesblock__video">
+								<a href="#"><img src="img/videoservice.jpg" alt=""></a>
+							</div>
+						</div>
+						<div class="examplesblock__right">
+							<h4>Съемка обзорного ролика о центре распределения</h4>
+							<p>Провели аэровидеосъемку распределительного центра в Парголово. Выполнили монтаж, цветокоррекцию и установили инфографику, согласно режиссерскому сценарию.</p>
+							<p>На этапе предпродакшена выполнен режиссерский сценарий, подготовлен аниматик для графики и примерная раскадровка.</p>
+							<div class="examplesblock__txtbottom">
+								<span class="budjet"><b>Бюджет:</b> до 250 000 руб.</span>
+								<span>Аэросъемка - AERO MOVIE</span>
+								<span>Оператор - Вадим Гусев; VFX - Васильев Илья; монтаж - Илья Клименко</span>
+							</div>
+						</div>
+					</div>
+					<div class="examplesblock__box">
+						<div class="examplesblock__left">
+							<div class="examplesblock__video">
+								<a href="#"><img src="img/videoservice.jpg" alt=""></a>
+							</div>
+						</div>
+						<div class="examplesblock__right">
+							<h4>Съемка обзорного ролика о центре распределения</h4>
+							<p>Провели аэровидеосъемку распределительного центра в Парголово. Выполнили монтаж, цветокоррекцию и установили инфографику, согласно режиссерскому сценарию.</p>
+							<p>На этапе предпродакшена выполнен режиссерский сценарий, подготовлен аниматик для графики и примерная раскадровка.</p>
+							<div class="examplesblock__txtbottom">
+								<span class="budjet"><b>Бюджет:</b> до 250 000 руб.</span>
+								<span>Аэросъемка - AERO MOVIE</span>
+								<span>Оператор - Вадим Гусев; VFX - Васильев Илья; монтаж - Илья Клименко</span>
+							</div>
+						</div>
+					</div>
+					<div class="examplesblock__box">
+						<div class="examplesblock__left">
+							<div class="examplesblock__video">
+								<a href="#"><img src="img/videoservice.jpg" alt=""></a>
+							</div>
+						</div>
+						<div class="examplesblock__right">
+							<h4>Съемка обзорного ролика о центре распределения</h4>
+							<p>Провели аэровидеосъемку распределительного центра в Парголово. Выполнили монтаж, цветокоррекцию и установили инфографику, согласно режиссерскому сценарию.</p>
+							<p>На этапе предпродакшена выполнен режиссерский сценарий, подготовлен аниматик для графики и примерная раскадровка.</p>
+							<div class="examplesblock__txtbottom">
+								<span class="budjet"><b>Бюджет:</b> до 250 000 руб.</span>
+								<span>Аэросъемка - AERO MOVIE</span>
+								<span>Оператор - Вадим Гусев; VFX - Васильев Илья; монтаж - Илья Клименко</span>
+							</div>
+						</div>
+					</div>
+					
+					<div class="examplesblock__button">
+						<a class="button button_1" href="#">Посмотреть еще</a>
+						<a href="#" class="video__lookmore">Остальные работы в разделе “Порфтолио”</a>
+					</div>	
+
+				</div>
+			</div>
+			
+			<div class="servicetxt">
+				<?php require_once('includes/textblock.php'); ?>
+			</div>
+			
+			<?php require_once('includes/want.php'); ?>	
+
+		</div>
+		<div class="footer">
+			<?php require_once('includes/footer.php'); ?>
+		</div>
+	</div>
+	</body>
 </html>
