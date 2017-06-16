e918ce81372bfe43099940368fe4bf6205520210
Berzeg07 <berezg86@gmail.com>
2017-06-16 12:41:15 +0400

    15.06

--- js/fonts.js
+++ js/fonts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	$("head").append('<link href="https://fonts.googleapis.com/css?family=Ubuntu:300,300i,400,400i,500,500i,700,700i" rel="stylesheet">');
 	$("head").append("<link href='//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css' rel='stylesheet'>");
 	$("head").append("<link href='https://api-maps.yandex.ru/2.1/?lang=tr_TR' rel='stylesheet'>");
+	$("head").append("<link href='https://www.youtube.com/player_api' rel='stylesheet'>");
 
 
 
