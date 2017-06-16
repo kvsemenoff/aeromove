d9117b95f13ed8c05575b6b62f41b5119df28ffc
kvsemenoff <kvsemenoff@mail.ru>
2017-06-16 15:12:16 +0400

    d

--- includes/popup.php
+++ includes/popup.php
@@ -22,9 +22,9 @@
 </div>
 
 
-	<div  class="modal-window" id="video-tube">
-		<span class="modal-close" id="stop">Закрыть</span>
-		<iframe width="100%" height="100%" id="player" src="https://www.youtube.com/embed/qQUzAFFUJng?version=3&enablejsapi=1&color=red&showinfo=0&vq=highres&modestbranding=1&buffering=0&loop=0&rel=0" allowfullscreen></iframe>
-	</div>
+<div  class="modal-window" id="video-tube">
+	<span class="modal-close" id="stop">Закрыть</span>
+	<iframe class="videoframe" id="player" src="https://www.youtube.com/embed/qQUzAFFUJng?version=3&enablejsapi=1&color=red&showinfo=0&vq=highres&modestbranding=1&buffering=0&loop=0&rel=0" allowfullscreen></iframe>
+</div>
 
 
--- includes/second_video.php
+++ includes/second_video.php
@@ -1,14 +1,8 @@
 <div class="container">
-
 	<div class="video-box">
 		<div class="video-box__item">
 			<a href="#video-tube" data-modal="modal"><img src="img/play.png" alt=""></a>
 			<p>Посмотрите наше портфолио за 2016 год!</p>
 		</div>
 	</div>
-
-<!-- 	<div class="video-tube">
-
-	</div> -->
-	
 </div>
