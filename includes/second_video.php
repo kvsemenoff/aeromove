03be44cdbe7dbda67685d77de101e02c2501224b
Berzeg07 <berezg86@gmail.com>
2017-06-16 14:28:19 +0400

    15.06

--- includes/second_video.php
+++ includes/second_video.php
@@ -2,17 +2,13 @@
 
 	<div class="video-box">
 		<div class="video-box__item">
-			<a href="#order" data-modal="modal"><img src="img/play.png" alt=""></a>
+			<a href="#video-tube" data-modal="modal"><img src="img/play.png" alt=""></a>
 			<p>Посмотрите наше портфолио за 2016 год!</p>
 		</div>
 	</div>
 
 	<div class="video-tube">
-		<div class="modal-window" id="order">
-
-			<span class="modal-close" id="stop">Х</span>
-			<iframe width="100%" height="100%" id="player" src="https://www.youtube.com/embed/qQUzAFFUJng?version=3&enablejsapi=1&color=red&showinfo=0&vq=highres&modestbranding=1&buffering=0&loop=0&rel=0" allowfullscreen></iframe>
-		</div>
+		<?php require_once('includes/popup.php'); ?>
 
 	</div>
 	
