f66edd50b84dc9ccb4f0252a578daea501e51253
engine77 <zaur.1kager@gmail.com>
2017-06-16 17:29:41 +0400

    aerophoto

--- aerovideo.php
+++ aerovideo.php
@@ -94,8 +94,6 @@
 						<li><a href="viewshootingtab2">Участие в съемках роликов</a></li>
 						<li><a href="viewshootingtab3">Ролики “под ключ”</a></li>
 					</ul>
-
-
 					<div class="clearfix"></div>
 					<div class="portfolio__block viewshootingtab1">					
 						<a href="#" class="portfolio__box">
@@ -265,14 +263,357 @@
 							</div>
 						</a>						
 					</div>
+
+					<div class="shooting__button whatweshot__button">
+						<a class="button button_1" href="#">Перейти в портфолио</a>
+					</div>
 				</div>
 			</div>
 
+			
+			<div class="costtheshooting aerovideo-osttheshooting">
+				<div class="container">
+					<div class="bigpark">
+						<h1 class="common-title">Стоимость проведения съемки и оборудования</h1>
+
+						<div class="bigpark__leftbox">
+							<div class="bigpark__tab bigparktab1">
+								<div class="bigpark__img">
+									<img src="img/copter1.png" alt="">
+								</div>
+								<div class="bigpark__txtbox">
+									<h4>Квадрокоптер <b>DJI PHANTOM 4</b></h4>
+									<span>любительский уровень</span>
+									<p>
+										Самый простой вариант для съемки. Рекомендуется для фотосъемки объектов
+										и территорий, съемки малобюджетных видеороликов. Управляется одним пилотом,
+										непригоден для съемки любой динамики, только простые пролеты.
+									</p>
+								</div>				
+							</div>
+							<div class="bigpark__tab bigparktab2">
+								<div class="bigpark__img">
+									<img src="img/copter1.png" alt="">
+								</div>
+								<div class="bigpark__txtbox">
+									<h4>Квадрокоптер <b>DJI INSPIRE ZENMUSE X3</b></h4>
+									<p>Самый простой вариант для съемки. Рекомендуется для фотосъемки объектов
+										и территорий, съемки малобюджетных видеороликов. Управляется одним пилотом,
+										непригоден для съемки любой динамики, только простые пролеты.
+									</p>
+								</div>				
+							</div>
+							<div class="bigpark__tab bigparktab3">
+								<div class="bigpark__img">
+									<img src="img/copter1.png" alt="">
+								</div>
+								<div class="bigpark__txtbox">
+									<h4>Квадрокоптер <b>DJI NSPIRE X5, X5R</b></h4>
+									<p>Самый простой вариант для съемки. Рекомендуется для фотосъемки объектов
+										и территорий, съемки малобюджетных видеороликов. Управляется одним пилотом,
+										непригоден для съемки любой динамики, только простые пролеты.
+									</p>
+								</div>				
+							</div>
+							<div class="bigpark__tab bigparktab4">
+								<div class="bigpark__img">
+									<img src="img/copter1.png" alt="">
+								</div>
+								<div class="bigpark__txtbox">
+									<h4>Квадрокоптер <b>DJI MATRICE 600 и  SONY ALPHA A4S II</b></h4>
+									<p>Самый простой вариант для съемки. Рекомендуется для фотосъемки объектов
+										и территорий, съемки малобюджетных видеороликов. Управляется одним пилотом,
+										непригоден для съемки любой динамики, только простые пролеты.
+									</p>
+								</div>				
+							</div>
+							<div class="bigpark__tab bigparktab5">
+								<div class="bigpark__img">
+									<img src="img/copter1.png" alt="">
+								</div>
+								<div class="bigpark__txtbox">
+									<h4>Квадрокоптер <b>DJI MATRICE 600 и REDот</b></h4>
+									<p>Самый простой вариант для съемки. Рекомендуется для фотосъемки объектов
+										и территорий, съемки малобюджетных видеороликов. Управляется одним пилотом,
+										непригоден для съемки любой динамики, только простые пролеты.
+									</p>
+								</div>				
+							</div>	
+							<div class="bigpark__button">
+								<a class="button button_1" href="#">Заказать видеосъемку</a>
+							</div>
+						</div>
+
+						<div class="bigpark__rightbox">
+							<h4>Список оборудования и стоимость:</h4>
+							<div class="bigpark__accordeon">
+								<div class="bigpark__accordeon-item" data-tab="bigparktab1">
+									<div class="bigpark__accordeon-img">
+										<img src="img/coptersm1.png" alt="">
+									</div>
+									<span class="bigpark__accordeon-nameproduct">DJI Phantom 4</span>
+									<div class="bigpark__accordeon-pricebox">
+										<span>от <b>5.000</b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+									</div>
+									<div class="clearfix"></div>
+									<div class="bigpark__accordeon-hiddenbox">
+										<div class="bigpark__accordeon-characteristics">
+											<div class="bigpark__accordeon-hourprice">
+												<em>Стоимость:</em>
+												<span>от 2 часов - <b>5 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+												<span>до 8 часов - <b>8 000 </b><i class="fa fa-rub" aria-hidden="true"></i></span>
+												<span>смена 8-10 часов - <b>10 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+											</div>
+											<div class="bigpark__accordeon-tech">
+												<em>Тех. характеристики:</em>
+												<span>объектив 20 mm;</span>
+												<span>видео FHD fps 120,</span>
+												<span>4K fps 50</span>
+											</div>
+										</div>
+										<div class="bigpark__accordeon-video">
+											<a href="#">
+												<img src="img/bigparkimg.jpg" alt="">
+											</a>
+										</div>													
+									</div>
+									<div class="clearfix"></div>
+								</div>
+								<div class="bigpark__accordeon-item" data-tab="bigparktab2">
+									<div class="bigpark__accordeon-img">
+										<img src="img/coptersm2.png" alt="">
+									</div>
+									<span class="bigpark__accordeon-nameproduct">DJi Inspire Zenmuse X3</span>
+									<div class="bigpark__accordeon-pricebox">
+										<span>от <b>10.000</b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+									</div>
+									<div class="clearfix"></div>
+									<div class="bigpark__accordeon-hiddenbox">
+										<div class="bigpark__accordeon-characteristics">
+											<div class="bigpark__accordeon-hourprice">
+												<em>Стоимость:</em>
+												<span>от 2 часов - <b>5 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+												<span>до 8 часов - <b>8 000 </b><i class="fa fa-rub" aria-hidden="true"></i></span>
+												<span>смена 8-10 часов - <b>10 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+											</div>
+											<div class="bigpark__accordeon-tech">
+												<em>Тех. характеристики:</em>
+												<span>объектив 20 mm;</span>
+												<span>видео FHD fps 120,</span>
+												<span>4K fps 50</span>
+											</div>
+										</div>
+										<div class="bigpark__accordeon-video">
+											<a href="#">
+												<img src="img/bigparkimg.jpg" alt="">
+											</a>
+										</div>												
+									</div>
+									<div class="clearfix"></div>	
+								</div>
+								<div class="bigpark__accordeon-item" data-tab="bigparktab3">
+									<div class="bigpark__accordeon-img">
+										<img src="img/coptersm3.png" alt="">
+									</div>
+									<span class="bigpark__accordeon-nameproduct">DJI NSPIRE X5, X5R</span>
+									<div class="bigpark__accordeon-pricebox">
+										<span>от <b>15.000</b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+									</div>
+									<div class="clearfix"></div>
+									<div class="bigpark__accordeon-hiddenbox">
+										<div class="bigpark__accordeon-characteristics">
+											<div class="bigpark__accordeon-hourprice">
+												<em>Стоимость:</em>
+												<span>от 2 часов - <b>5 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+												<span>до 8 часов - <b>8 000 </b><i class="fa fa-rub" aria-hidden="true"></i></span>
+												<span>смена 8-10 часов - <b>10 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+											</div>
+											<div class="bigpark__accordeon-tech">
+												<em>Тех. характеристики:</em>
+												<span>объектив 20 mm;</span>
+												<span>видео FHD fps 120,</span>
+												<span>4K fps 50</span>
+											</div>
+										</div>
+										<div class="bigpark__accordeon-video">
+											<a href="#">
+												<img src="img/bigparkimg.jpg" alt="">
+											</a>
+										</div>												
+									</div>
+									<div class="clearfix"></div>	
+								</div>
+								<div class="bigpark__accordeon-item" data-tab="bigparktab4">
+									<div class="bigpark__accordeon-img">
+										<img src="img/coptersm4.png" alt="">
+									</div>
+									<span class="bigpark__accordeon-nameproduct">DJI MATRICE 600 <i> и </i> SONY ALPHA A4S II</span>
+									<div class="bigpark__accordeon-pricebox">
+										<span>от <b>15.000</b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+									</div>
+									<div class="clearfix"></div>
+									<div class="bigpark__accordeon-hiddenbox">
+										<div class="bigpark__accordeon-characteristics">
+											<div class="bigpark__accordeon-hourprice">
+												<em>Стоимость:</em>
+												<span>от 2 часов - <b>5 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+												<span>до 8 часов - <b>8 000 </b><i class="fa fa-rub" aria-hidden="true"></i></span>
+												<span>смена 8-10 часов - <b>10 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+											</div>
+											<div class="bigpark__accordeon-tech">
+												<em>Тех. характеристики:</em>
+												<span>объектив 20 mm;</span>
+												<span>видео FHD fps 120,</span>
+												<span>4K fps 50</span>
+											</div>
+										</div>
+										<div class="bigpark__accordeon-video">
+											<a href="#">
+												<img src="img/bigparkimg.jpg" alt="">
+											</a>
+										</div>											
+									</div>
+									<div class="clearfix"></div>		
+								</div>
+								<div class="bigpark__accordeon-item" data-tab="bigparktab5">
+									<div class="bigpark__accordeon-img">
+										<img src="img/coptersm4.png" alt="">
+									</div>
+									<span class="bigpark__accordeon-nameproduct">DJI matrice 600 <i>и</i> red</span>
+									<div class="bigpark__accordeon-pricebox">
+										<span>от <b>15.000</b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+									</div>
+									<div class="clearfix"></div>
+									<div class="bigpark__accordeon-hiddenbox">
+										<div class="bigpark__accordeon-characteristics">
+											<div class="bigpark__accordeon-hourprice">
+												<em>Стоимость:</em>
+												<span>от 2 часов - <b>5 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+												<span>до 8 часов - <b>8 000 </b><i class="fa fa-rub" aria-hidden="true"></i></span>
+												<span>смена 8-10 часов - <b>10 000 </b> <i class="fa fa-rub" aria-hidden="true"></i></span>
+											</div>
+											<div class="bigpark__accordeon-tech">
+												<em>Тех. характеристики:</em>
+												<span>объектив 20 mm;</span>
+												<span>видео FHD fps 120,</span>
+												<span>4K fps 50</span>
+											</div>
+										</div>
+										<div class="bigpark__accordeon-video">
+											<a href="#">
+												<img src="img/bigparkimg.jpg" alt="">
+											</a>
+										</div>											
+									</div>
+									<div class="clearfix"></div>	
+								</div>
+							</div>							
+						</div>
+
+						<div class="clearfix"></div>
+					</div>
+				</div>
+			</div>	
+
+			<div class="compare aerovideo__compare">
+				<div class="container">
+					<h1 class="common-title">Сравнение видео с разных камер</h1>
+					<div class="compare__block">
+
+						<div class="compare__leftbox">
+							<a href="#" class="compare__imgbox comparetab1">
+								<img src="img/compare1.jpg" alt="">
+							</a>
+							<a href="#" class="compare__imgbox comparetab2">
+								<img src="img/compare1.jpg" alt="">
+							</a>
+							<a href="#" class="compare__imgbox comparetab3">
+								<img src="img/compare1.jpg" alt="">
+							</a>					
+
+							<div class="compare__tabs aerovideo__tabs">
+								<a href="comparetab1" class="compare__tabs-activelink">
+									<b>DJi Inspire X3</b>
+									<span>4000х2800 рх, 72 pdpi</span>
+								</a>
+								<a href="comparetab2">
+									<b>Canon 5D Mark III</b>
+									<span>5800х3900 рх, 240 pdpi</span>
+								</a>
+								<a href="comparetab3">
+									<b>DJi Matrice</b>
+									<span>c камерой RED</span>
+								</a>
+							</div>
+						</div>
+
+						<div class="compare__rightbox">
+							<p>По этой <a href="#">ссылке</a> вы можете скачать примеры снимков со всех камер и детальней рассмотреть качество фотографий.</p>
+							<p>Рекомендуем приблизить каждый снимок и вы сразу поймете основное отличие, если технические характеристики вам ничего не говорят. При выборе оборудования для аэрофотосъемки нужно отталкиваться от целей и задач. Свяжитесь с нами и мы поможем выбрать самый оптимальный вариант.</p>
+							<p>Так же акцентируем внимание на том, что параметр dpi всегда можно искусственно увеличить, если будет такая задача.</p>
+							<div class="compare__button">
+								<a class="button button_1" href="#">Заказать звонок специалиста</a>
+							</div>	
+						</div>
+					</div>
+				</div>
+			</div>	
+
+			<div class="howwework">
+				<div class="container">
+					<h1 class="common-title">Как мы работаем</h1>
+				</div>
+				<div class="howwework__slider">
+					<div class="item">
+						<div class="howwework__img">
+							<img src="img/panoram1.jpg" alt="">
+						</div>
+						<p class="howwework__txtbottom">Проведение съемки в горах на высоте 235 метров над землей.</p>
+					</div>
+					<div class="item">
+						<div class="howwework__img">
+							<img src="img/panoram1.jpg" alt="">
+						</div>
+						<p class="howwework__txtbottom">Проведение съемки в горах на высоте 235 метров над землей.</p>
+					</div>
+					<div class="item">
+						<div class="howwework__img">
+							<img src="img/panoram1.jpg" alt="">
+						</div>
+						<p class="howwework__txtbottom">Проведение съемки в горах на высоте 235 метров над землей.</p>
+					</div>
+					<div class="item">
+						<div class="howwework__img">
+							<img src="img/panoram1.jpg" alt="">
+						</div>
+						<p class="howwework__txtbottom">Проведение съемки в горах на высоте 235 метров над землей.</p>
+					</div>
+					<div class="item">
+						<div class="howwework__img">
+							<img src="img/panoram1.jpg" alt="">
+						</div>
+						<p class="howwework__txtbottom">Проведение съемки в горах на высоте 235 метров над землей.</p>
+					</div>
+					<div class="item">
+						<div class="howwework__img">
+							<img src="img/panoram1.jpg" alt="">
+						</div>
+						<p class="howwework__txtbottom">Проведение съемки в горах на высоте 235 метров над землей.</p>
+					</div>
+					<div class="item">
+						<div class="howwework__img">
+							<img src="img/panoram1.jpg" alt="">
+						</div>
+						<p class="howwework__txtbottom">Проведение съемки в горах на высоте 235 метров над землей.</p>
+					</div>
+				</div>				
+			</div>
 
 
-
-
-
+			<div class="servicetxt">
+				<?php require_once('includes/textblock.php'); ?>
+			</div>
+			<?php require_once('includes/want.php'); ?>	
 
 
 
