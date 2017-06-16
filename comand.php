f2844715eb0103a75dba59da27aaaad62204edac
boka <web_masters_07@mail.ru>
2017-06-16 17:27:29 +0300

    comand

--- comand.php
+++ comand.php
@@ -8,6 +8,51 @@
 	<div class="wrapper">
 		<div class="content">
 			<?php require_once('includes/header_top.php'); ?>
+			<?php require_once('includes/header_bottom2.php'); ?>
+			<div class="comand">
+				<div class="container">
+					<div class="comand-block">
+						<h2>Аэросъемка. Основной состав</h2>
+						<div class="col-md-3">
+							<div class="comand1">
+								<img src="img/comand.jpg" alt="img">
+							</div>
+							<p>
+								Намоконов Денис<br>
+								<span>Видеооператор на коптере</span>
+							</p>
+						</div>
+						<div class="col-md-3">
+							<div class="comand2">
+								<img src="img/comand.jpg" alt="img">
+							</div>
+							<p>
+								Намоконов Денис<br>
+								<span>Видеооператор на коптере</span>
+							</p>
+						</div>
+						<div class="col-md-3">
+							<div class="comand3">
+								<img src="img/comand.jpg" alt="img">
+							</div>
+							<p>
+								Намоконов Денис<br>
+								<span>Видеооператор на коптере</span>
+							</p>
+						</div>
+						<div class="col-md-3">
+							<div class="comand4">
+								<img src="img/comand.jpg" alt="img">
+							</div>
+							<p>
+								Намоконов Денис<br>
+								<span>Видеооператор на коптере</span>
+							</p>
+						</div>
+						<div class="clearfix"></div>
+					</div>		
+				</div>
+			</div>
 			<?php require_once('includes/want.php'); ?>
 		</div>
 		<div class="footer">
