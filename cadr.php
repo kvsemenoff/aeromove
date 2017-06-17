cdc95ca08d948f916e720a3dd3690f7c6bc702c3
Berzeg07 <berezg86@gmail.com>
2017-06-17 15:17:36 +0400

    17.06

--- cadr.php
+++ cadr.php
@@ -79,8 +79,114 @@
 					</div>
 					<div class="clearfix"></div>
 
+							<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+						<a href="#">
+							<div class="cadr-box">
+
+								<div class="cadr-box__img">
+									<img class="img-responsive" src="img/sl.jpg" alt="">
+								</div>
+								<div class="cadr-box__txt">
+									<span>04 мая 2017</span>
+									<p>Кадры из т/с  “5 минут тишины”</p>
+									<span class="vision">Смотреть</span>
+								</div>
+
+							</div>
+						</a>
+
+					</div>
+
+					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+						<a href="#">
+							<div class="cadr-box">
+								
+								<div class="cadr-box__img">
+									<img class="img-responsive" src="img/sl.jpg" alt="">
+								</div>
+								<div class="cadr-box__txt">
+									<span>04 мая 2017</span>
+									<p>Кадры из т/с  “5 минут тишины”</p>
+									<span class="vision">Смотреть</span>
+								</div>
+								
+							</div>
+						</a>
+					</div>
+					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+						<a href="#">
+							<div class="cadr-box">
+								
+								<div class="cadr-box__img">
+									<img class="img-responsive" src="img/sl.jpg" alt="">
+								</div>
+								<div class="cadr-box__txt">
+									<span>04 мая 2017</span>
+									<p>Кадры из т/с  “5 минут тишины”</p>
+									<span class="vision">Смотреть</span>
+								</div>
+								
+							</div>
+						</a>
+					</div>
+					<div class="clearfix"></div>
+
+							<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+						<a href="#">
+							<div class="cadr-box">
+
+								<div class="cadr-box__img">
+									<img class="img-responsive" src="img/sl.jpg" alt="">
+								</div>
+								<div class="cadr-box__txt">
+									<span>04 мая 2017</span>
+									<p>Кадры из т/с  “5 минут тишины”</p>
+									<span class="vision">Смотреть</span>
+								</div>
+
+							</div>
+						</a>
+
+					</div>
+
+					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+						<a href="#">
+							<div class="cadr-box">
+								
+								<div class="cadr-box__img">
+									<img class="img-responsive" src="img/sl.jpg" alt="">
+								</div>
+								<div class="cadr-box__txt">
+									<span>04 мая 2017</span>
+									<p>Кадры из т/с  “5 минут тишины”</p>
+									<span class="vision">Смотреть</span>
+								</div>
+								
+							</div>
+						</a>
+					</div>
+					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+						<a href="#">
+							<div class="cadr-box">
+								
+								<div class="cadr-box__img">
+									<img class="img-responsive" src="img/sl.jpg" alt="">
+								</div>
+								<div class="cadr-box__txt">
+									<span>04 мая 2017</span>
+									<p>Кадры из т/с  “5 минут тишины”</p>
+									<span class="vision">Смотреть</span>
+								</div>
+								
+							</div>
+						</a>
+					</div>
+					<div class="clearfix"></div>
+
 				</div>
 			</div>
+
+			<?php require_once('includes/want.php'); ?>
 
 		</div>
 
