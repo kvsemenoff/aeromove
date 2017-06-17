ebc46bb7776835f9e810a9585f6a810dd4e36ae0
Berzeg07 <berezg86@gmail.com>
2017-06-17 12:36:46 +0400

    17.06

--- cadr.php
+++ cadr.php
@@ -4,14 +4,92 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
+
 	<?php require_once('includes/header.php'); ?>
+
 	<div class="wrapper">
 		<div class="content">
-			<?php require_once('includes/header-top.php'); ?>
+
+			<?php require_once('includes/header_top.php'); ?>
+			<?php require_once('includes/header_bottom2.php'); ?>
+
+			<div class="container">
+
+
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<div class="cadr-tab">
+							<ul class="portfolio__tab">
+								<li><a href="viewshootingtab1" class="panorams__acivetab">По дате</a></li>
+								<li><a href="viewshootingtab2">По популярности</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+
+					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+						<a href="#">
+							<div class="cadr-box">
+
+								<div class="cadr-box__img">
+									<img class="img-responsive" src="img/sl.jpg" alt="">
+								</div>
+								<div class="cadr-box__txt">
+									<span>04 мая 2017</span>
+									<p>Кадры из т/с  “5 минут тишины”</p>
+									<span class="vision">Смотреть</span>
+								</div>
+
+							</div>
+						</a>
+
+					</div>
+
+					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+						<a href="#">
+							<div class="cadr-box">
+								
+								<div class="cadr-box__img">
+									<img class="img-responsive" src="img/sl.jpg" alt="">
+								</div>
+								<div class="cadr-box__txt">
+									<span>04 мая 2017</span>
+									<p>Кадры из т/с  “5 минут тишины”</p>
+									<span class="vision">Смотреть</span>
+								</div>
+								
+							</div>
+						</a>
+					</div>
+					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+						<a href="#">
+							<div class="cadr-box">
+								
+								<div class="cadr-box__img">
+									<img class="img-responsive" src="img/sl.jpg" alt="">
+								</div>
+								<div class="cadr-box__txt">
+									<span>04 мая 2017</span>
+									<p>Кадры из т/с  “5 минут тишины”</p>
+									<span class="vision">Смотреть</span>
+								</div>
+								
+							</div>
+						</a>
+					</div>
+					<div class="clearfix"></div>
+
+				</div>
+			</div>
+
 		</div>
+
 		<div class="footer">
+
 			<?php require_once('includes/footer.php'); ?>
+
 		</div>
 	</div>
+
 </body>
 </html>
