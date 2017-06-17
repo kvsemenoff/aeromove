e995b20c43f8233a4e988e8894b8124b08e873f3
boka <web_masters_07@mail.ru>
2017-06-17 13:57:24 +0300

    services

--- services.php
+++ services.php
@@ -8,10 +8,172 @@
 	<div class="wrapper">
 		<div class="content">
 			<?php require_once('includes/header_top.php'); ?>
+			<?php require_once('includes/header_bottom2.php'); ?>
+			<div class="shooting aeropanoramabg">	
+				<div class="container">
+					<div class="shooting_block">				
+						<div class="shooting__leftbox">
+							<h1 class="common-title">Условия аренды</h1>
+							<div class="shooting__txtbox">
+								<p>Видеосъемка с воздуха — наша основная специализация. У нас есть коптеры, способные решать разные задачи и с разным бюджетом.</p>
+								<p>Небольшие квадрокоптеры для съемок малобюджетных роликов, тяжелый гексакоптер с профессиональными камерами и каосильный коптер с подвесом на камеры линейки RED, который принимает участие
+									в съемках большого кино.</p>
+								</div>
+								<div class="shooting__button">
+									<a class="button button_1" href="#">Арендовать квадрокоптер</a>
+								</div>
+							</div>
+							<div class="shooting__rightbox">
+								<div class="common-slider">
+									<div class="slider-owl-carousel">						
+										<div class="item">
+											<div class="common-slider__img">
+												<img src="img/sliderimg1.jpg" alt="">
+											</div>			
+										</div>
+										<div class="item">
+											<div class="common-slider__img">
+												<img src="img/sliderimg1.jpg" alt="">
+											</div>			
+										</div>
+										<div class="item">
+											<div class="common-slider__img">
+												<img src="img/sliderimg1.jpg" alt="">
+											</div>			
+										</div>
+										<div class="item">
+											<div class="common-slider__img">
+												<img src="img/sliderimg1.jpg" alt="">
+											</div>			
+										</div>
+										<div class="item">
+											<div class="common-slider__img">
+												<img src="img/sliderimg1.jpg" alt="">
+											</div>			
+										</div>
+										<div class="item">
+											<div class="common-slider__img">
+												<img src="img/sliderimg1.jpg" alt="">
+											</div>			
+										</div>		
+										<div class="item">
+											<div class="common-slider__img">
+												<img src="img/sliderimg1.jpg" alt="">
+											</div>			
+										</div>
+										<div class="item">
+											<div class="common-slider__img">
+												<img src="img/sliderimg1.jpg" alt="">
+											</div>			
+										</div>
+										<div class="item">
+											<div class="common-slider__img">
+												<img src="img/sliderimg1.jpg" alt="">
+											</div>			
+										</div>				
+									</div>
+									<div class="common-slider__txtbox">
+										<div class="common-slider__numberbox">
+											<span class="number-active">1</span>
+											<i>/</i>
+											<span class="number-all">9</span>
+										</div>
+										<p class="common-slider__txt">Проведение аэрофотосъемки ЖК в Сертолово для компании “Балтйский лизинг”</p>
+									</div>
+									<div class="clearfix"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="shooting">	
+					<div class="container">
+						<div class="col-md-12">
+							<h1 class="services-h1">Мастер-класс по аэросъемки</h1>
+						</div>
+						<div class="clearfix"></div>
+						<div class="col-md-6">
+							<div class="services">
+								<div class="slider-owl-carousel">						
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>		
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>
+									<div class="item">
+										<div class="common-slider__img">
+											<img src="img/sliderimg1.jpg" alt="">
+										</div>			
+									</div>				
+								</div>
+								<div class="common-slider__txtbox">
+									<div class="common-slider__numberbox">
+										<span class="number-active">1</span>
+										<i>/</i>
+										<span class="number-all">9</span>
+									</div>
+									<p class="common-slider__txt services-text">Проведение аэрофотосъемки ЖК в Сертолово для компании “Балтйский лизинг”</p>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="shooting__txtbox services-p">
+								<p>Видеосъемка с воздуха — наша основная специализация. У нас есть коптеры, способные решать разные задачи и с разным бюджетом.</p>
+								<p>Небольшие квадрокоптеры для съемок малобюджетных роликов, тяжелый гексакоптер с профессиональными камерами и каосильный коптер с подвесом на камеры линейки RED, который принимает участиев съемках большого кино.</p>
+								<p>Видеосъемка с воздуха — наша основная специализация. У нас есть коптеры, способные решать разные задачи и с разным бюджетом.</p>
+							</div>
+							<div class="shooting__button">
+								<a class="button button_1" href="#">Заказать мастер-класс</a>
+							</div>
+						</div>
+						
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="servicetxt">
+					<?php require_once('includes/textblock.php'); ?>
+				</div>
+				<?php require_once('includes/want.php'); ?>
+			</div>
+			<div class="footer">
+				<?php require_once('includes/footer.php'); ?>
+			</div>
 		</div>
-		<div class="footer">
-			<?php require_once('includes/footer.php'); ?>
-		</div>
-	</div>
-</body>
-</html>
+	</body>
+	</html>
