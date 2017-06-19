fd09a8b1f23cb7120a73bc05f154b3c8acae7df1
kvsemenoff <kvsemenoff@mail.ru>
2017-06-19 14:12:27 +0400

    e

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <!doctype html>
 <html lang="ru">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Главная</title>
-<?php require_once('includes/header.php'); ?>
-<?php require_once('includes/header_top.php'); ?>
-<?php require_once('includes/header_bottom.php'); ?>
-<?php require_once('includes/second_menu.php'); ?>
-<?php require_once('includes/video.php'); ?>
-<?php require_once('includes/big.php'); ?>
-<?php require_once('includes/second_video.php'); ?>
-<?php require_once('includes/more.php'); ?>
-<?php require_once('includes/our.php'); ?>
-<?php require_once('includes/want.php'); ?>
-<?php require_once('includes/footer.php'); ?>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Главная</title>
+	<?php require_once('includes/header.php'); ?>
+	<div class="wrapper">
+		<div class="content">
+			<?php require_once('includes/header.php'); ?>
+			<?php require_once('includes/header_top.php'); ?>
+			<?php require_once('includes/header_bottom.php'); ?>
+			<?php require_once('includes/second_menu.php'); ?>
+			<?php require_once('includes/video.php'); ?>
+			<?php require_once('includes/big.php'); ?>
+			<?php require_once('includes/second_video.php'); ?>
+			<?php require_once('includes/more.php'); ?>
+			<?php require_once('includes/our.php'); ?>
+			<?php require_once('includes/want.php'); ?>
+		</div>
+		<div class="footer">
+			<?php require_once('includes/footer.php'); ?>
+		</div>
+	</div>
 </body>
 </html>
