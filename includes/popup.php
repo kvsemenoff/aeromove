f1e0b1622b51894eda75c12e0257fad735dc1f00
boka <web_masters_07@mail.ru>
2017-06-21 10:23:45 +0300

    form

--- includes/popup.php
+++ includes/popup.php
@@ -24,6 +24,31 @@
 <div class="modal-window" id="order">
 	<span class="modal-close">Закрыть</span>
 	<!-- Здесь форма -->
+	<form action="#" method="post" class="want-form">
+			<h3 class="want-form__title">Оставьте свой номер и специалист вас проконсультирует</h3>
+			<div class="wants-first">
+				<label class="want-form__label">Имя</label><br>
+				<input type="text" name="uname">
+			</div>
+			<div class="wants-two">
+				<label class="want-form__label">Телефон</label><br>
+				<input  type="text" name="phone"><br>
+			</div>
+			<div class="clearfix"></div>
+			<div class="wants-three">
+				<label class="want-form__label">Комментарий</label><br>
+				<textarea name="comment"></textarea>
+			</div>
+			<div class="wants-four">
+				<label class="checkbox">
+					<input class="checkbox__hidden" type="checkbox" name="checkbox-test" checked="checked">
+					<span class="checkbox__custom"></span>
+					<span class="checkbox__label">Я согласен(на) с политикой конфиденциальности</span>
+				</label>
+				<span class="want-form__text">Обработка персональных данных осуществляется согласно ФЗ-РФ от 27 июля 2006 г. №152 ФЗ.</span><br>
+				<input type="submit" name="submit" value="Оставить заявку" class="want-form__submit">
+			</div>
+		</form>
 </div>
 
 
